2136abfb5f4165f05e6c80a0a745df61fcd947ec
Thitipon <thitiphon.s@ku.th>
2021-02-01 19:02:00 +0700

    lab1_7

---
+++ Lab1_7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    float time;
+    char n;
+    int h,m;
+    scanf("%f",&time);
+     if(time>=0&&time<=11.59)
+        n = 'a';
+    else if(time>=12&&time<=23.59)
+        n = 'p';
+    h = (int)time;
+    time = time * 100;
+    m = (int)time % 100;
+    if(m==0)
+        printf("%d:%d0 %c.m.",h,m,n);
+    else
+        printf("%d:%d %c.m.",h,m,n);
+}
