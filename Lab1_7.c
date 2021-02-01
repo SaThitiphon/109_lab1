da433b86459662883e6b1e01dc7888fabedadc98
Thitipon <thitiphon.s@ku.th>
2021-02-01 19:11:09 +0700

    update

--- Lab1_7.c
+++ Lab1_7.c
@@ -5,19 +5,24 @@ int main()
     char n;
     int h,m;
     scanf("%f",&time);
-     if(time>=0&&time<=11.59)
+     if((time>=0&&time<=11.59)||time>=24)
         n = 'a';
     else if(time>=12&&time<=23.59)
         n = 'p';
+    else if(time==24.00)
     h = (int)time;
     time = time * 100;
     m = (int)time % 100;
-    if(h>12)
+    if(h>12&&h<24)
     {
         h = h - 12;
     }
+    else if(h==24)
+        h = h - 24;
     if(m==0)
         printf("%d:%d0 %c.m.",h,m,n);
+    else if(h==0)
+        printf("0%d:%d %c.m.",h,m,n);
     else
         printf("%d:%d %c.m.",h,m,n);
 }
